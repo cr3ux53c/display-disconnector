d73da96348f634464ec4a3e88a9c952d8e3367b2
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-04-27 11:59:38 +0900

    새 프로젝트 생성

--- ScreenHalt/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <Windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
-	return 0;
-}
